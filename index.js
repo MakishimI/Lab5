acc9eed965360083278d88e05b2d92e77951a417
divin <8465721@stud.kai.edu.ua>
2025-05-04 13:08:57 +0300

    feat: реалізовано функцію завантаження конфігурації

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+const fs = require('fs/promises');
+
+async function loadConfig(filename) {
+    try {
+        const data = await fs.readFile(filename, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Помилка завантаження конфігурації:", error);
+        process.exit(1);
+    }
+}
